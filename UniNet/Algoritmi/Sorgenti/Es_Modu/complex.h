eb4cca212641a02b8ba8c072ca2a764182daf717
E88-Uni <enricobaldasso88@gmail.com>
2024-10-30 16:59:01 +0100

    Delete UniNet/Algoritmi directory

--- UniNet/Algoritmi/Sorgenti/Es_Modu/complex.h
+++
@@ -1,9 +0,0 @@
-typedef struct {
-    double re, im;
-} complex;
-complex new(double, double);
-complex add(complex, complex);
-complex sub(complex, complex);
-complex mult(complex, complex);
-complex quot(complex, complex);
-double mod(complex);
